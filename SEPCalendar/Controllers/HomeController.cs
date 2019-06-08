16d148319c4a0cbce17c1288150f6475d49e7f34
T153866 - Ngo Hieu Phat - K21T03 <phatngo3@vanlanguni.vn>
2019-06-08 18:43:05 +0700

    aa

---
+++ SEPCalendar/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEPCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetEvents()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+    }
+}
